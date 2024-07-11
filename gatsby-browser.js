e86b0153138e0f7cf06f4ee8db27119884363a11
Zahin Huq <47957498+zhuq3417@users.noreply.github.com>
2024-07-11 19:41:09 -0400

    test

--- gatsby-browser.js
+++
@@ -1,7 +0,0 @@
-import './src/styles/global.css'
-import React from "react"
-import Layout from "./src/components/layout"
-
-export const wrapPageElement = ({ element, props }) => {
-  return <Layout {...props}>{element}</Layout>
-}
